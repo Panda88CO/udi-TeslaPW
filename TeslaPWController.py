62495fafd2f01b49b0eb08d8015a782f3672143e
Panda88CO <65139210+Panda88CO@users.noreply.github.com>
2023-02-15 18:07:22 -0800

    0.2.00

--- TeslaPWController.py
+++ TeslaPWController.py
@@ -385,7 +385,7 @@ if __name__ == "__main__":
     try:
         #logging.info('Starting Tesla Power Wall Controller')
         polyglot = udi_interface.Interface([])
-        polyglot.start('0.1.33')
+        polyglot.start('0.2.00')
         polyglot.updateProfile()
         polyglot.setCustomParamsDoc()
         TeslaPWController(polyglot, 'controller', 'controller', 'TeslaPowerWall')
